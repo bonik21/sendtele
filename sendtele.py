cb63b0c231e81f46c56a0abbe547e37ff94a6872
BoniK <mail@bonik.me>
2022-07-13 12:00:59 +0900

    Update sendtele.py

--- sendtele.py
+++ sendtele.py
@@ -1,15 +1,14 @@
-# 샘플 Python 스크립트입니다.
-# 우분투 pip3 install python-telegram-bot
-import telegram
+# pip3 install python-telegram-bot
+# import telegram
 import sys
 import configparser
-# 우분투 추가
 import os
+import requests
 
 # info
 sendtele_version = 1.0
 print('sendtele 버전:', sendtele_version)
-# 윈도우 
+# 윈도우 (exe)
 #sendtele_path = sys.executable[:-13]
 # 우분투
 sendtele_path = os.path.dirname(os.path.realpath(__file__))
@@ -21,18 +20,20 @@ config.read(f'{sendtele_path}/sendtele.ini')
 settings = config['SETTINGS']
 my_telegram_token = settings["telegram_token"]
 my_chat_id = settings["chat_id"]
-bot = telegram.Bot(token=my_telegram_token)
 
 if len(sys.argv) >= 2:
-    val1 = sys.argv[1]
-    print('telegram_token :', my_telegram_token)
+    message = sys.argv[1]
     print('chat_id :', my_chat_id)
-    print('메시지 내용 :', val1)
+    print('메시지 내용 :', message)
 else:
     print('메시지 내용이 없습니다.')
     print('사용법 : python3 sendtele.py "메시지 내용"')
     sys.exit()
 
-# 메시지 전송
-message = val1
-bot.sendMessage(chat_id=my_chat_id, text=f"{message}")
+def send_msg(text):
+   token = my_telegram_token
+   chat_id = my_chat_id
+   url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text 
+   results = requests.get(url_req)
+
+send_msg(message)
